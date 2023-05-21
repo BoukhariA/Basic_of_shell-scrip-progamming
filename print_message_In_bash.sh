8d7b5c734d94b15dbac345e60857ef800298bced
BoukhariA <92463486+BoukhariA@users.noreply.github.com>
2023-05-21 22:44:25 +0530

    Add files via upload

---
+++ print_message_In_bash.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+#print a simple message which a String
+
+#to something in bash use echo
+
+echo "I am learning shell script programming"
+
+#this is how you print message in bash sometime echo used without the double quotes "" it will fine 
+#but I think the best practice to use them "".
+
+echo I am learning shell script programming
+
+#echo can be used to print interger as well as fload 
+
+echo 9
+
+echo 999.99
---
+++ variable.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+echo "\n#Variable are a bit complicated compare to other progarmming language
+#It might seem easy to declare a variable in here, but trust me they aren't fun to work with
+#To declare a variable, Variable=value' it can uppercase, lowercase mix of both
+
+#Notice (Important)
+there is not space between the variable and the equal sign (=) and the value that you assign to the a variable, if you did space between them it will show an error, be carefull"
+
+
+echo "\n"
+number=19
+
+Number=21
+
+floatNumber=199.99
+
+Name=Aboud
+
+#to print a variable you can use echo and $ sin to assign the variable value.
+
+echo $number 
+
+echo $Number
+
+# Multiple variable can be printed in the same time
+ 
+echo "\nMultiple variable printed at the same time"
+
+echo $number $Number $Name $floatNumber
+
+
+#sentence best practices use quotes it is better
+echo "\n"
+
+LongString="Aboud is learning shell script programming"
+
+echo $LongString
+
